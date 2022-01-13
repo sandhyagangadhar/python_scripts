3bfe1e4c312e611c623974e9aac5a9f511e5d3ce
Sandhya <32234542+sandhyagangadhar@users.noreply.github.com>
2022-01-13 15:04:24 +0530

    Update main.py

--- linearregression/main.py
+++ linearregression/main.py
@@ -27,16 +27,16 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 # Visualising the Training set results
-plt.scatter(X_train, y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.scatter(X_train, y_train, color = 'blue')
+plt.plot(X_train, regressor.predict(X_train), color = 'green')
 plt.title('Bandgap vs Pressure (Training set)')
 plt.xlabel('Pressure')
 plt.ylabel('Bandgap')
 plt.show()
 
 # Visualising the Test set results
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.scatter(X_test, y_test, color = 'blue')
+plt.plot(X_train, regressor.predict(X_train), color = 'green')
 plt.title('Bandgap vs Pressure (Test set)')
 plt.xlabel('Pressure')
 plt.ylabel('Bandgap')
